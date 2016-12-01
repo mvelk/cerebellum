887d516675d69f199de55d08e46f3703d6de2372
pi <pv97@cornell.edu>
2016-11-30 16:49:12 -0800

    start ui sync

--- bundle.js
+++ bundle.js
@@ -53,11 +53,22 @@
 	
 	
 	document.addEventListener("DOMContentLoaded", () => {
-	  // create elements
-	
+	  // get elements
 	  let canvas = document.getElementById("myCanvas");
 	  let ctx = canvas.getContext("2d");
+	  let learningRateEl = document.getElementById("learning-rate");
+	  let iterationsEl = document.getElementById("iterations");
+	  let activationFEl = document.getElementById("activation-function");
+	  // let addLayerButton = document.getElementById("");
+	  // let removeLayerButton = document.getElementById("");
+	
+	  // set model variables
+	  let interval = undefined;
 	  let sampleSize = 250;
+	  let neuralNet = [2,4,2,1];
+	  let learningRate = learningRateEl.value;
+	  let iterations = iterationsEl.value;
+	  let activationF = activationFEl.value;
 	
 	  // set img source
 	  let image = document.createElement("img");
@@ -74,16 +85,14 @@
 	  // define domain size
 	  let domainDim = 100;
 	
-	  // set play variables
-	  let interval = undefined;
-	  let iterations = 0;
-	
 	  // on image load, draw image then sample pixel data
 	  image.addEventListener("load", function () {
-	    ctx.drawImage(image, 0, 0, canvasDim, canvasDim);
-	    let dataset = getImageData(ctx, sampleSize, canvasDim, canvasDim);
-	    let model = new Model([2,4,2,1], dataset, dataset, "tanh", 0.1);
-	    window.model = model;
+	
+	    const reset = () => {
+	      ctx.drawImage(image, 0, 0, canvasDim, canvasDim);
+	      let dataset = getImageData(ctx, sampleSize, canvasDim, canvasDim);
+	      let model = new Model(neuralNet, dataset, dataset, "tanh", 0.1);
+	    }
 	
 	    let diagram = sankeyDiagram()
 	      .width(1200)
@@ -93,15 +102,23 @@
 	      .linkTypeTitle(function(d) { return d.data.title; })
 	      .linkColor(function(d) { return d.data.color; });
 	
+	    let heatMap = new HeatMap(canvasDim, sampleSize, domainDim, d3.select("#heatmap"));
+	
 	    let data = model.getSankeyData();
 	    d3.select('#sankey')
 	      .datum(data)
 	      .call(diagram);
 	
-	    let heatMap = new HeatMap(canvasDim, sampleSize, domainDim, d3.select("#heatmap"));
 	    heatMap.generate();
 	    heatMap.paintGradient(model);
 	
+	
+	    // model actions
+	    const reset = () => {
+	
+	    }
+	
+	    //controls
 	    const play = () => {
 	      interval = window.setInterval(increment, 1)
 	    }
